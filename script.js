9e90d807402f53d7fd9d3cbded61821eedb3217e
inessa <inessa2209@yahoo.com>
2021-12-11 00:10:57 -0500

    added css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const h3 = document.createElement("h3");
+h3.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h3);
 
