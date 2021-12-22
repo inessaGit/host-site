9b308a1f4954802264f96ac668dc889bc24f7aea
inessa <inessa2209@yahoo.com>
2021-12-22 13:58:32 -0500

    two pointers

--- leetcode/string/reverseString.js
+++ leetcode/string/reverseString.js
@@ -1,11 +1,48 @@
 /*
 https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1183/
 */
-
+//Approach 1: string to array; array reverse; array to string
 var reverseString = function(s) {
     s=Array.from(s).reverse().join("");
-
     console.log(s);
 };
 
-reverseString("hello");
+//Approach 2: Using loop starting from end of the string 
+var reverseString1=function(s){
+  let reversed ="";
+  let end = s.length-1; 
+  let start =0;
+
+  for (i=end;i>=0;i--){
+    reversed+=s.charAt(i);
+  }
+  console.log(reversed);
+  return reversed;
+}
+/*Approach 3: Two pointers
+one starts from the first element and another starts from the last element. 
+Continue swapping the elements until the two pointers meet each other.
+
+Not sure why it is accepted on leetcode??
+*/
+var reverseString2=function(s){
+    let start =0;
+    let end =s.length-1;
+    //let reversed ="";
+   
+     while(start<end){
+       let temp =s[start];
+       s[start]=s[end];
+       s[end]=temp;
+
+       start++
+       end--;
+     }
+   
+    console.log(s)
+    return s;
+
+}
+
+reverseString2("hello");
+//reverseString("12344")
---
+++ string/stringImmutable.js
@@ -0,0 +1,6 @@
+let s ="hello";
+s[1]="20";
+console.log(s) //hello 
+
+s=s[1]+s;
+console.log(s)
