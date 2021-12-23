036f652af8bb2cf11e26f1569775bc08eb591d59
inessa <inessa2209@yahoo.com>
2021-12-23 11:47:14 -0500

    added

--- educative/string/reverseWords.js
+++ educative/string/reverseWords.js
@@ -6,11 +6,10 @@ https://www.educative.io/module/lesson/data-structures-in-javascript/3YmYvqV7VNO
 
 let reverseWords = function(sentence) {     // sentence here is an array of characters
      
-    let n=Array.from(sentence).reverse().join("");
-    console.log(n)
+    
     return sentence;
   };
 
-   let s="We love Javascript";
+   let s="We love Javascript";//Exp Javascript love We	
    let m ="Hello World"
   reverseWords(s)	;
