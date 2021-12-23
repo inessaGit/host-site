64e73e70c0b7651445e6ae0a685174bac99fc43a
inessa <inessa2209@yahoo.com>
2021-12-23 14:14:40 -0500

    shuffle string

---
+++ leetcode/string/shuffleString.js
@@ -0,0 +1,11 @@
+/*
+https://leetcode.com/problems/shuffle-string/
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+*/
+
+var restoreString = function(s, indices) {
+    
+};
