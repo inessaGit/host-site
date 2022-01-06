b183a4177eb3ae643eed17064a2281472a33f3d9
inessa <inessa2209@yahoo.com>
2022-01-06 00:16:16 -0500

    leetcode string

--- array/arrayRemove.js
+++ array/arrayRemove.js
@@ -1,3 +1,11 @@
+/*
+Modify array:
+- delete elements with modifying array.length
+- delete elements with splice(index,delCount, insertEl)
+- delete el with pop() from end; with shift from beginning;
+*/
+
+
 const removeFromArray = function(arr,...targets) {
     let res =arr.filter((el)=> !targets.includes(el));
     return res;
@@ -19,3 +27,10 @@ res = arr1.filter(item => !arr2.includes(item));
 
 
 console.log(removeFromArray([1,2,3,4],3,4));
+
+//Decreasing the length property does, however, delete elements.
+let arr=[1,2,3,4,6];
+arr.length=4; //[1234]
+console.log(arr)
+arr.shift();
+console.log(arr) //[234]
