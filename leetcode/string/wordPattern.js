f5dd93523d7e720f7287276fb0b4c13ebc3ba26d
inessa <inessa2209@yahoo.com>
2022-02-02 10:10:12 -0500

    leetcode

--- leetcode/string/wordPattern.js
+++ leetcode/string/wordPattern.js
@@ -1,13 +1,26 @@
 
 /*
 https://leetcode.com/problems/word-pattern/submissions/
+
+public boolean wordPattern(String pattern, String str) {
+    String[] words = str.split(" ");
+    if (words.length != pattern.length())
+        return false;
+    Map index = new HashMap();
+    for (Integer i=0; i<words.length; ++i)
+        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+            return false;
+    return true;
+}
 */
 
+//02/02 wrong solution -> change 
 var wordPattern = function(pattern, s) {
     let arr =s.split(' ');
     let map = new Map(); 
     let n =arr.length; 
-    
+    if (pattern.length !=s.length) return false; 
+
     for (let i=0;i <n;i++){
         if(!map.has(pattern[i])){
             map.set(pattern[i],arr[i]);
