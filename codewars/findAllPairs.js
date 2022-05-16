2a71ca7a6facee74e252a7b1063547fd7c9489f2
inessa <inessa2209@yahoo.com>
2022-05-16 09:42:38 -0400

    codewars

--- codewars/findAllPairs.js
+++ codewars/findAllPairs.js
@@ -13,15 +13,34 @@ Random tests: maximum array length is 1000, range of values in array is between 
 
 function duplicates(array){
     let ans =0;
-    if (array.length==0 || array.length ==1) return ans; 
-    
-    let set = new Set(array); 
-    ans =array.length-set.size; 
-
+    if (array.length<=1) return ans; 
+    // let set = new Set(array);  cannot handle dup pairs of dups e.g [0,0,0,0]
+    let map = new Map(); 
+   for (let el of array){
+      map.set(el,map.has(el)? map.get(el)+1:1);
+   }
+   for (let [k,v] of map.entries()){
+       v>1 ? ans+=Math.floor(v/2): ans;
+   } 
+   //count map values where v>1 ans+=Math.floor(v:2)
     console.log(`ans=${ans}`) ;
     return ans; 
     }
 
+    function duplicates1(array){
+          if (newArray.length <= 1) return 0;
+          const newArray = array.sort((a,b) => a-b);
+          let count = 0;
+          for (let i = 0; i < newArray.length ; i++) {
+            if (newArray[i] == newArray[i+1]) {
+              count++;
+              i++;
+            }
+          }
+          return count;
+        }
     let a =[1, 2, 5, 6, 5, 2] ; //exp 2
     duplicates(a);
-    duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]); 
+    duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]); //exp 4
+    duplicates([0, 0, 0, 0, 0, 0, 0]); //exp 3 
+
